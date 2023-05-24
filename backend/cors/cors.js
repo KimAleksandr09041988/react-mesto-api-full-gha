1d95c356d2cead2d58051fe3040a1cd776bc452d
Kim Aleksandr <a.kim@jelata.tech>
2023-05-24 16:35:53 +0700

    15pr hot fix 15

--- backend/cors/cors.js
+++ backend/cors/cors.js
@@ -6,24 +6,22 @@ const allowedCors = [
   'http://localhost:3000',
 ];
 
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
 // eslint-disable-next-line consistent-return
-const cors = (req, res, next) => {
-  const { method } = req;
+module.exports = (req, res, next) => {
   const { origin } = req.headers;
+  const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
-  res.header('Access-Control-Allow-Credentials', true);
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Origin', '*');
   }
+
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
-  next();
-};
 
-module.exports = cors;
+  return next();
+};
