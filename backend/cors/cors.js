2c56c7025a877ac50d9c63b441f9eeca812aebdd
Kim Aleksandr <a.kim@jelata.tech>
2023-05-24 15:07:53 +0700

    15pr hot fix 10

--- backend/cors/cors.js
+++ backend/cors/cors.js
@@ -16,6 +16,7 @@ const cors = (req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
